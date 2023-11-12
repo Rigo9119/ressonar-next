30368e55d169b815e28f4675f9a04b302c905354
Rigo Armando Rosero Castillo <rigo@192.168.68.108>
2023-11-12 15:18:28 -0500

    integrates dato cms and adds first layer content

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,33 @@
+import { performRequest } from "../../lib/datocms";
+
 export const metadata = {
     title: "Inicio",
 };
 
-export default function Home() {
-    return <main className="ml-16">home</main>;
+const HERO_QUERY = `
+    query hero {
+        hero {
+            heroTittle
+            heroParagraph
+        }
+    }
+`;
+
+export default async function Home() {
+    const {
+        data: {
+            hero: { heroTittle, heroParagraph },
+        },
+    } = await performRequest({ query: HERO_QUERY });
+
+    return (
+        <main>
+            <div className="ml-16 h-screen bg-black flex items-center justify-center flex-col">
+                <h1 className="text-white montserrat text-6xl mb-12">
+                    {heroTittle}
+                </h1>
+                <p className="text-white text-center w-3/4">{heroParagraph}</p>
+            </div>
+        </main>
+    );
 }
