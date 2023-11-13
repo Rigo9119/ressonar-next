dc96796aced5b60bd2f629879111594300719e46
Rigo Armando Rosero Castillo <rigo@192.168.68.107>
2023-11-13 16:54:46 -0500

    adds contact form to the website

--- src/app/contacto/page.js
+++ src/app/contacto/page.js
@@ -1,19 +1,48 @@
 export const metadata = {
-    title: 'Contacto'
-}
+    title: "Contacto",
+};
 
 export default function Page() {
     return (
-        <main className="h-screen flex justify-center items-center ml-16">
-            <div>
-                <form className="flex flex-col">
-                    <input type="text" placeholder="Tú nombre" />
-                    <input type="text" placeholder="Tú correo" />
-                    <input type="text" placeholder="Tú telefono" />
-                    <textarea placeholder="Escribenos como podemos ayudarte" />
-                    <input type="submit" />
-                </form>
+        <main className="ml-16 h-screen flex justify-center items-center">
+            <div className="flex flex-col gap-6 justify-center items-center">
+                <h3 className="font-bold text-start text-4xl">Contacto</h3>
+                <div className="w-1/2">
+                    <p className="text-left">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Facilis praesentium alias modi consequatur dolorem
+                        aperiam quisquam ipsam provident obcaecati, nemo non quo
+                        maxime.
+                    </p>
+                </div>
+                <div className="w-1/2">
+                    <form className="flex flex-col gap-4 justify-center items-center">
+                        <input
+                            className="px-4 py-2 w-full border rounded border-color-black"
+                            type="text"
+                            placeholder="Tú nombre"
+                        />
+                        <input
+                            className="px-4 py-2 w-full border rounded border-color-black"
+                            type="text"
+                            placeholder="Tú correo"
+                        />
+                        <input
+                            className="px-4 py-2 w-full border rounded border-color-black"
+                            type="text"
+                            placeholder="Tú telefono"
+                        />
+                        <textarea
+                            className="px-4 py-2 w-full border rounded border-color-black"
+                            placeholder="Escribenos como podemos ayudarte"
+                        />
+                        <input
+                            className="border w-1/2 rounded bg-green-500 px-4 py-2"
+                            type="submit"
+                        />
+                    </form>
+                </div>
             </div>
         </main>
-    )
+    );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -23,23 +23,33 @@ export default async function Home() {
 
     return (
         <main className="ml-16">
-            <div className="h-screen bg-black flex items-center justify-center flex-col">
+            <section className="h-screen bg-black flex items-center justify-center flex-col">
                 <h1 className="text-white text-6xl mb-12">
                     {heroTittle}
                 </h1>
                 <p className="text-white text-xl text-center w-3/4">
                     {heroParagraph}
                 </p>
-            </div>
+            </section>
+            <section>
+                <div>
+                    <h2>
+                        Lo que hacemos
+                    </h2>
+                    <p>
+                        Desarrollamos todo el proceso audiovisual, desde la escritura de guion y conceptualización, preproducción, filmación y postproducción.
+
+                        Traemos a la vida aquello que aún está por contar.
+                    </p>
+                </div>
+                <div>
+                    <img src="" alt="imagen de algo" />
+                </div>
+            </section>
             <section className="px-4 py-20 flex flex-row justify-around items-center h-auto bg-gray-600">
                 <PackItemCard />
                 <PackItemCard />
                 <PackItemCard />
-            </section>
-            <section>
-                <h2>
-                    Lo que hacemos
-                </h2>
             </section>
             <section>
                 <h2>
