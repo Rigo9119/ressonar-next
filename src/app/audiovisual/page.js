7a93dfc78516fe4833530718796ad7e023736648
Rigo Armando Rosero Castillo <rigo@192.168.68.106>
2023-11-21 21:02:15 -0500

    add content to the audiovisual page

--- src/app/audiovisual/page.js
+++ src/app/audiovisual/page.js
@@ -1,24 +1,50 @@
-export const metadata ={
-    title: 'Audiovisual'
+/* eslint-disable @next/next/no-img-element */
+import { performRequest } from "../../../lib/datocms";
+
+export const metadata = {
+    title: "Audiovisual",
 };
 
-export default function Page() {
+const AUDIOVISUAL_QUERY = `
+    query audiovisual {
+        allAudiovisuals {
+            title
+            link(markdown: false)
+            asset {
+                url
+            }
+        }
+    }
+`;
+
+export default async function Page() {
+    const {
+        data: { allAudiovisuals },
+    } = await performRequest({ query: AUDIOVISUAL_QUERY });
+
     return (
         <main className="pt-16 md:ml-16 md:pt-0">
-            <div className="h-screen flex flex-col md:flex-row md:flex-wrap">
-                <div className="w-full h-80 md:h-auto md:w-1/2 flex items-center justify-center  bg-green-200">
-                    Item 1
-                </div>
-                <div className="w-full h-80 md:h-auto md:w-1/2 flex items-center justify-center  bg-green-300">
-                    Item 2
-                </div>
-                <div className="w-full h-80 md:h-auto md:w-1/2 flex items-center justify-center  bg-green-400">
-                    Item 3
-                </div>
-                <div className="w-full h-80 md:h-auto md:w-1/2 flex items-center justify-center  bg-green-500">
-                    Item 4
-                </div>
+            <div className="flex flex-wrap w-auto items-center justify-between">
+                {allAudiovisuals.reverse().map((item, index) => {
+                    return (
+                        <a
+                            key={index}
+                            href={item.link}
+                            target="_blank"
+                            className="relative w-1/2"
+                        >
+                            <img
+                                className="blur-sm hover:blur-none"
+                                src={`${item.asset.url}`}
+                                alt={`${item.title}`}
+                            />
+                            <span className="absolute bottom-1/2 left-1/2 text-4xl text-white">
+                                {item.title}
+                            </span>
+                        </a>
+                    );
+                })}
             </div>
         </main>
-    )
-};
+    );
+}
