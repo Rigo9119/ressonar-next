93582fd9a2bf93273bb766db0ce2424f4809a025
Rigo Armando Rosero Castillo <rigo@192.168.68.107>
2023-11-23 17:54:31 -0500

    adds more space to the perfil component

--- src/components/perfil/perfil.jsx
+++ src/components/perfil/perfil.jsx
@@ -9,7 +9,7 @@ const Perfil = ({ perfil }) => {
     return (
         <>
             {toggle ? (
-                <div className="order-first w-full p-4 bg-red-500 text-black flex flex-col gap-4 md:w-1/4 md:order-last">
+                <div className="order-first w-full p-4 bg-red-500 text-black flex flex-col justify-between gap-4 md:w-1/4 md:order-last">
                     <div className="flex flex-col gap-4 justify-around items-center md:items-start">
                         <h3 className="text-white uppercase">
                             {perfil.nombre}
@@ -27,7 +27,7 @@ const Perfil = ({ perfil }) => {
                             </ul>
                         </div> */}
                     </div>
-                    <div className="flex items-center justify-between w-full h-1/4">
+                    <div className="flex items-center justify-between w-full">
                         <Link className="text-xl" href="/talento">Volver</Link>
                         <button className="hidden md:flex" onClick={handletoggle}>
                             <span className="text-white text-xl">&rarr;</span>
